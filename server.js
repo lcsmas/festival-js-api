32662e639c2bc374cb9eac9af1572ba3efb67103
Lucas <lucas_mas@msn.com>
2018-06-03 12:53:08 +0200

    create fest

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post("/api/v1/festivals", cors(), (req, res) => {
 		if (err) {
 		  console.error('connection error', err.stack)
 		} else {
-		  console.log(`POST ${req.path}, params : ${req.originalUrl}`)
+		  console.log(`POST ${req.path}, body : ${req.body}`)
 		}
 	});	  
 	res.json('lel');
