85db3ebd1b93af33cce8a77cc6138e64bdcbc2aa
Lucas <lucas_mas@msn.com>
2018-06-03 09:57:13 +0200

    cors

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ var corsOptions = {
 }
 
 app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.set('port', port);
 
  //Active le Cross-Origin-Request
