ec8ae9d923b9639caa383d1d76bce532ba9f10c6
Lucas <lucas_mas@msn.com>
2018-06-03 11:08:37 +0200

    test app.use

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ var express = require('express'),
 
 app.use(cors());
 
+app.use( (res,req) => {
+	let log = req.path;
+	console.log(req.path);
+});
+
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
